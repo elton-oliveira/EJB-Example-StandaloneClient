5789a28a8e67bf58f0168f8bc8840045e997d5ef
elton-oliveira <elton.pmp@gmail.com>
2013-11-22 22:36:11 -0200

    Create ejb client project

---
+++ src/br/com/fluentcode/ejb/client/CalculatorClient.java
@@ -0,0 +1,17 @@
+package br.com.fluentcode.ejb.client;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import br.com.fluentcode.ejb.remote.CalculatorRemote;
+
+public class CalculatorClient {
+	
+	public static void main(String[] args) throws NamingException {
+		InitialContext ctx = new InitialContext();
+		CalculatorRemote calculator = (CalculatorRemote) ctx.lookup("EJB-Example/CalculatorBean!br.com.fluentcode.ejb.remote.CalculatorRemote");
+		int value = calculator.add(2, 3);
+		System.out.println(value);
+	}
+
+}
