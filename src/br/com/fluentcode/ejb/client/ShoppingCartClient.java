4b47f3af5fa211d17b5085b12bfdd17d568ce273
elton-oliveira <elton.pmp@gmail.com>
2013-11-23 16:40:19 -0200

    Add stateful session bean client

---
+++ src/br/com/fluentcode/ejb/client/ShoppingCartClient.java
@@ -0,0 +1,21 @@
+package br.com.fluentcode.ejb.client;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import br.com.fluentcode.ejb.remote.ShoppingCartRemote;
+
+public class ShoppingCartClient {
+	
+	public static void main(String[] args) throws NamingException {
+		InitialContext ctx = new InitialContext();
+		ShoppingCartRemote cart =  (ShoppingCartRemote) ctx.lookup("EJB-Example/ShoppingCartBean!br.com.fluentcode.ejb.remote.ShoppingCartRemote");
+		cart.addItem("Pen");
+		cart.addItem("Notebook");
+		for(String item : cart.getItems()){
+			System.out.println(item);
+		}
+		cart.finishShopping();
+	}
+
+}
--- src/br/com/fluentcode/ejb/remote/CalculatorRemote.java
+++ src/br/com/fluentcode/ejb/remote/CalculatorRemote.java
@@ -1,14 +1,7 @@
 package br.com.fluentcode.ejb.remote;
 
-import javax.ejb.Remote;
 
-/**
- * 
- * TODO Preciso tirar o classpath target runtime do jboss-7.
- * Para compilar preciso ver como resolver com o ant e ivy (adicionar um lib com scope de ...)
- *
- */
-@Remote
+
 public interface CalculatorRemote {
 	
 	int add(int x, int y);
