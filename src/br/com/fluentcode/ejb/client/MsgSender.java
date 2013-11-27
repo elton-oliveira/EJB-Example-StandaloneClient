77395980fdb62c54c4a324dc111befce6ef818e6
elton-oliveira <elton.pmp@gmail.com>
2013-11-27 18:10:15 -0200

    Create jms sender

---
+++ src/br/com/fluentcode/ejb/client/MsgSender.java
@@ -0,0 +1,27 @@
+package br.com.fluentcode.ejb.client;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+public class MsgSender {
+	
+	public static void main(String[] args) throws Exception {
+		InitialContext context = new InitialContext();
+		Destination destination = (Destination) context.lookup("FluentCodeQueue");
+		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("jms/RemoteConnectionFactory");
+		Connection connection = connectionFactory.createConnection("elton","123");
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		MessageProducer messageProducer = session.createProducer(destination);
+		TextMessage textMessage = session.createTextMessage();
+		textMessage.setText("Planning is essential!");
+		messageProducer.send(textMessage);
+		System.out.println("Message sent successfully!");
+		connection.close();
+	}
+
+}
