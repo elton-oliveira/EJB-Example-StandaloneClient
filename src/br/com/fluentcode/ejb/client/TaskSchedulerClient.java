df4007afd0c53b71244b7204944d57650a8b97ed
elton-oliveira <elton.pmp@gmail.com>
2013-12-01 14:22:39 -0200

    Add arg to shedule task client

--- src/br/com/fluentcode/ejb/client/TaskSchedulerClient.java
+++ src/br/com/fluentcode/ejb/client/TaskSchedulerClient.java
@@ -1,6 +1,7 @@
 package br.com.fluentcode.ejb.client;
 
 import java.util.Calendar;
+import java.util.List;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -12,13 +13,18 @@ public class TaskSchedulerClient {
 	public static void main(String[] args) throws NamingException {
 		InitialContext ctx = new InitialContext();
 		TaskSchedulerRemote scheduler =  (TaskSchedulerRemote) ctx.lookup("EJB-Example/TaskSchedulerBean!br.com.fluentcode.ejb.remote.TaskSchedulerRemote");
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.SECOND, 5);
-		for(int i = 1; i<= 50; i++){
-			calendar.add(Calendar.MILLISECOND, 100);
-			scheduler.scheduleTask(calendar.getTime(), "Planning is essential! (Task "+i+")");
+		if("scheduleTask".equals(args[0])){
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.SECOND, 15);
+			for(int i = 1; i<= 50; i++){
+				calendar.add(Calendar.MILLISECOND, 100);
+				scheduler.scheduleTask(calendar.getTime(), "("+i+")");
+			}
+			System.out.println("The task was scheduled");
+		}else if("cancelTask".equals(args[0])){
+			List<String> messages = scheduler.cancelTask();
+			System.out.println(messages);
 		}
-		System.out.println("The task was scheduled");
 	}
 
 }
